c455f9c71d1818de4e63201e49c44d7976095fe6
atha <74566464+Pandademic@users.noreply.github.com>
2023-04-30 19:38:47 -0400

    update info in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='newm',
-      version='0.3',
-      description='newm - Wayland compositor',
-      url="https://github.com/jbuchermn/newm",
-      author='Jonas Bucher',
-      author_email='j.bucher.mn@gmail.com',
+setup(name='newm-atha',
+      version='0.4',
+      description='newm-atha - touchpad and touchscreen centric wayland compositor',
+      url="https://sr.ht/~atha/newm-atha",
+      author='~atha',
+      author_email='74566464+Pandademic@users.noreply.github.com',
       packages=['newm', 'newm.helper', 'newm.resources', 'newm.overlay', 'newm.widget', 'newm.dbus', 'newm.gestures', 'newm.gestures.provider', 'newm_panel_basic'],
       package_data={'newm.resources': ['wallpaper.jpg', 'newm.desktop']},
       scripts=['bin/start-newm', 'bin/.start-newm', 'bin/newm-cmd', 'bin/newm-panel-basic'],
